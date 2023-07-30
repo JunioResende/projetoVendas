9e01571e377cb5305b9183b062a493013e550b1d
JunioResende <junioresende23@gmail.com>
2023-07-30 20:54:30 -0300

    start

---
+++ index.py
@@ -0,0 +1,28 @@
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+app = dash.Dash(__name__)
+server = app.server
+
+
+# =========Layout===========
+app.layout = html.Div(
+    children=[]
+)
+
+# =========Callbacks========
+
+
+
+
+
+#=========Run Server=========
+if __name__ == '__main__':
+    app.run_server(port=8050, debug=True)
