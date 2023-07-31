2c6c0ea375ce7587d02ebc72458484c61cdb37c3
JunioResende <junioresende23@gmail.com>
2023-07-30 21:42:40 -0300

    preLayout

--- index.py
+++ index.py
@@ -15,18 +15,35 @@ server = app.server
 df_data = pd.read_csv('supermarket_sales.csv')
 df_data['Date'] = pd.to_datetime(df_data['Date'])
 
+# =========Layout===========
+app.layout = html.Div(
+    children=[
+        html.H5('Cidades:'),
+        dcc.Checklist(
+                        df_data['City'].value_counts().index,
+                        df_data['City'].value_counts().index,
+                        id='checkCity'
+                        ),
+        
+        html.H5('Variavel de analise'),
+        dcc.RadioItems(
+            ['gross income', 'Rating'], 
+            'gross income', 
+            id='mainVariable'
+            ),
+        
+        dcc.Graph(id='cityFig'),
+        dcc.Graph(id='payFig'),
+        dcc.Graph(id='incomePerProductFig'),
+    ]
+)
 
-# # =========Layout===========
-# app.layout = html.Div(
-#     children=[]
-# )
-
-# # =========Callbacks========
+# =========Callbacks========
 
 
 
 
 
-# #=========Run Server=========
-# if __name__ == '__main__':
-#     app.run_server(port=8050, debug=True)
+#=========Run Server=========
+if __name__ == '__main__':
+    app.run_server(port=8050, debug=True)
